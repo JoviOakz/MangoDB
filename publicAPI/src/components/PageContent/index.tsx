1db1382dce417e1ae5429f2bb47dcb2feb895af2
JoviOakz <joao.mateussdc@gmail.com>
2024-09-09 11:48:41 -0300

    feat: api tests

--- publicAPI/src/components/PageContent/index.tsx
+++ publicAPI/src/components/PageContent/index.tsx
@@ -1,10 +1,21 @@
-import Button from "../Button";
+import { useEffect } from "react";
+import axios from "axios";
+// import Button from "../Button";
 
 const PageContent = () => {
+    useEffect(() => {
+        handleGet();
+    }, []);
+    async function handleGet() {
+        const res = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+        console.log(res);
+    }
+
     return (
         <div className="w-full h-[95vh] flex justify-center items-center">
             <div className="w-1/2 h-1/2 bg-blue-200 rounded-2xl flex justify-center items-center shadow-md shadow-slate-400">
-                <Button title="Get Weather" />
+                <button onClick={handleGet}>a</button>
+                {/* <Button title="Get Weather" /> */}
             </div>
         </div>
     )
