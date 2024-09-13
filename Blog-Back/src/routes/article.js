95ec36a9668b4237d047cedf34e279d59d509afe
JoviOakz <joao.mateussdc@gmail.com>
2024-09-13 09:27:13 -0300

    Blog-Backend

---
+++ Blog-Back/src/routes/article.js
@@ -0,0 +1,7 @@
+const ArticleController = require('../controller/ArticleController');
+const route = express.Router();
+
+route
+    .post('/', ArticleController.create)
+    
+module.exports = route;
